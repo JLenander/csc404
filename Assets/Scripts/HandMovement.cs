9a20e1f469ba0aac60fc374e002f98b3477c320c
Viceu <66227626+Viceu@users.noreply.github.com>
2025-09-18 20:54:18 -0400

    renaming, comments, fixedupdate movement

--- Assets/Scripts/HandMovement.cs
+++ Assets/Scripts/HandMovement.cs
@@ -4,60 +4,51 @@ using UnityEngine.InputSystem;
 public class HandMovement : MonoBehaviour
 {
     public float speed = 5f;
-    private Vector2 moveInput;
-    private Rigidbody rb;
+    private Rigidbody _rb;
 
-    private InputAction moveAction;
-    private InputAction dpadAction;
+    private InputAction _moveAction;
+    private InputAction _dpadAction;
     public Vector3 movement;
-    bool disable = false;
+    private bool _disable;
 
-    private bool isMoving = false;
+    private bool _isMoving;
     public AudioSource moveSource;
     public AudioSource stopSource;
 
     private GameObject _currPlayer;
 
-    void Start()
+    private void Start()
     {
-        rb = GetComponent<Rigidbody>();
-        // audioSource = GetComponent<AudioSource>();
-
-        // moveAction = InputSystem.actions.FindAction("Move");
-        // moveAction.Enable();
-        // dpadAction = InputSystem.actions.FindAction("DpadMove");
+        _rb = GetComponent<Rigidbody>();
     }
 
-    void Update()
+    private void Update()
     {
-        if (disable)
+        if (_disable)
         {
-            // Vector2 move = dpadAction.ReadValue<Vector2>();
-            // rb.linearVelocity = move * speed;
-
-            Vector2 stickMove = moveAction.ReadValue<Vector2>();
-            Vector2 dpadMove = dpadAction.ReadValue<Vector2>();
+            Vector2 stickMove = _moveAction.ReadValue<Vector2>();
+            Vector2 dpadMove = _dpadAction.ReadValue<Vector2>();
             Vector3 stickMovement = new Vector3(-1 * stickMove.x, stickMove.y, 0);
             Vector3 dpadMovement = new Vector3(0, 0, dpadMove.y) * -1;
             movement = (stickMovement + dpadMovement) * speed;
-            rb.linearVelocity = movement;
-
-
+            // movement done in FixedUpdate
+            
             bool movingNow = movement.magnitude > 0.5f;
 
             // Movement started
-            if (movingNow && !isMoving)
+            if (movingNow && !_isMoving)
             {
-                isMoving = true;
+                _isMoving = true;
 
+                // != expensive but confirmed the right approach
                 if (moveSource != null && !moveSource.isPlaying)
                     moveSource.Play();
             }
 
             // Movement stopped
-            if (!movingNow && isMoving)
+            if (!movingNow && _isMoving)
             {
-                isMoving = false;
+                _isMoving = false;
 
                 if (moveSource != null && moveSource.isPlaying)
                     moveSource.Stop();
@@ -69,28 +60,27 @@ public class HandMovement : MonoBehaviour
 
     }
 
+    // Use FixedUpdate for physics-based movement
+    private void FixedUpdate()
+    {
+        _rb.MovePosition(_rb.position + movement * Time.fixedDeltaTime);
+    }
+
+    
+    // using TurnOn to initialize when player starts using the hand, not in Start() when object instantiate
     public void TurnOn(GameObject playerUsing)
     {
         _currPlayer = playerUsing;
         var input = _currPlayer.GetComponent<PlayerInput>();
-        moveAction = input.actions.FindAction("Move");
-        dpadAction = input.actions.FindAction("DpadMove");
-        disable = true;
+        _moveAction = input.actions.FindAction("Move");
+        _dpadAction = input.actions.FindAction("DpadMove");
+        _disable = true;
     }
 
     public void TurnOff(GameObject playerUsing)
     {
-        disable = false;
+        _disable = false;
     }
-
-    // public void OnMove(InputAction.CallbackContext context)
-    // {
-    //     moveInput = context.ReadValue<Vector2>();
-    //     Debug.Log("moving!");
-    // }
-    // private void FixedUpdate()
-    // {
-    //     rb.linearVelocity = movement * speed * Time.fixedDeltaTime;
-    // }
+    
 }
 

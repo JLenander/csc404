35ada106055b667ec508890588a53c7bb2a9774f
Viceu <66227626+Viceu@users.noreply.github.com>
2025-09-21 11:25:42 -0400

    setup phone and tested screen switch

--- Assets/Scripts/PhoneUIController.cs
+++ Assets/Scripts/PhoneUIController.cs
@@ -6,13 +6,31 @@ public class PhoneUIController : MonoBehaviour
     public Image screenImage; // drag UI Image here
     public Sprite faceIDSprite;
     public Sprite homeSprite;
-    public Sprite swipeSprite;
-    public Sprite matchSprite;
+    public Sprite swipeOneSprite;
+    public Sprite matchOneSprite;
+    public Sprite swipeTwoSprite;
+    public Sprite matchTwoSprite;
   
     public void ShowFaceID() => screenImage.sprite = faceIDSprite;
     public void ShowHome() => screenImage.sprite = homeSprite;
-    public void ShowSwipe() => screenImage.sprite = swipeSprite;
-    public void ShowMatch() => screenImage.sprite = matchSprite;
+
+    public void ShowSwipe()
+    {
+        if (screenImage.sprite == homeSprite)
+            screenImage.sprite = swipeOneSprite;
+        else if (screenImage.sprite == swipeOneSprite)
+            screenImage.sprite = swipeTwoSprite;
+        else if (screenImage.sprite == swipeTwoSprite)
+            screenImage.sprite = swipeOneSprite;
+    }
+
+    public void ShowMatch()
+    {
+        if (screenImage.sprite == swipeOneSprite)
+            screenImage.sprite = matchOneSprite;
+        else if (screenImage.sprite == swipeTwoSprite)
+            screenImage.sprite = matchTwoSprite;
+    }
 }
 
 

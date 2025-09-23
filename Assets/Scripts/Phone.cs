96589d9eb9d36ab4f5cb72df061602eff20f2cdc
unknown <j.chen1904@gmail.com>
2025-09-23 01:12:11 -0400

    arm debug

--- Assets/Scripts/Phone.cs
+++ Assets/Scripts/Phone.cs
@@ -6,6 +6,7 @@ public class Phone : InteractableObject
     private Vector3 ogPosition;
     private Quaternion ogRotation;
     private Transform parent;
+    private bool _canPickup;
 
     protected override void Start()
     {
@@ -13,17 +14,19 @@ public class Phone : InteractableObject
         ogPosition = transform.localPosition;
         ogRotation = transform.localRotation;
         parent = transform.parent;
+        _canPickup = true;
     }
 
     public override void InteractWithHand(Transform obj)
     {
-        if (canInteract)
+        if (_canPickup)
         {
             // move to hand
             DisableOutline();
             transform.parent = obj;
             transform.localPosition = new Vector3(0.0f, 5.2f, -1.0f);
             transform.localRotation = Quaternion.Euler(-88f, 10f, 0f);
+            _canPickup = false;
             Debug.Log("pickup success");
         }
     }
@@ -34,5 +37,6 @@ public class Phone : InteractableObject
         transform.parent = parent;
         transform.localPosition = ogPosition;
         transform.localRotation = ogRotation;
+        _canPickup = true;
     }
 }

dc788f1dce26bd3a8c5084e8c6a026dae5086800
Viceu <66227626+Viceu@users.noreply.github.com>
2025-09-22 23:08:39 -0400

    matching main

--- Assets/Scripts/HandConsole.cs
+++ Assets/Scripts/HandConsole.cs
@@ -1,8 +1,8 @@
 using UnityEngine;
-
 public class HandConsole : Interactable
 {
     public GameObject handRigTarget;
+    private bool _canInteract = true;
 
     void Start()
     {
@@ -11,13 +11,21 @@ public class HandConsole : Interactable
 
     public override void Interact(GameObject player)
     {
+        if (!_canInteract) return;  // check if there is already a player on the console
         player.GetComponent<Player>().TurnOff();
         handRigTarget.GetComponent<HandMovement>().TurnOn(player);
+        _canInteract = false;
     }
 
     public override void Return(GameObject player)
     {
         player.GetComponent<Player>().TurnOn();
         handRigTarget.GetComponent<HandMovement>().TurnOff(player);
+        _canInteract= true; // current player leaves
+    }
+
+    public override bool CanInteract()
+    {
+        return _canInteract;
     }
 }

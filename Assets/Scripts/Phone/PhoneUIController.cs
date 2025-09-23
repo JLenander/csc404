ee7e701c4f6afaf275d5c0ca4b31aa82152cba2a
Viceu <66227626+Viceu@users.noreply.github.com>
2025-09-23 03:13:10 -0400

    added any swipe screens and allowed shuffle

--- Assets/Scripts/Phone/PhoneUIController.cs
+++ Assets/Scripts/Phone/PhoneUIController.cs
@@ -10,9 +10,15 @@ public class PhoneUIController : MonoBehaviour
     public Sprite faceIDSprite;
     public Sprite homeSprite;
     public Sprite swipeOneSprite;
+    public Sprite anyOneSprite;
     public Sprite matchOneSprite;
     public Sprite swipeTwoSprite;
+    public Sprite anyTwoSprite;
     public Sprite matchTwoSprite;
+    // public Sprite swipeThreeSprite;      // for now no option to choose on third
+    // either swiped out fro first time (to one) or auto match on swipe
+    public Sprite anyThreeSprite;
+    public Sprite matchThreeSprite;
   
     public void ShowFaceID() => screenImage.sprite = faceIDSprite;
     public void ShowHome() => screenImage.sprite = homeSprite;
@@ -22,20 +28,34 @@ public class PhoneUIController : MonoBehaviour
     public void ShowSwipe()
     {
         if (screenImage.sprite == homeSprite)
-            screenImage.sprite = swipeOneSprite;
+            screenImage.sprite = anyOneSprite;
+        else if (screenImage.sprite ==anyOneSprite)
+            screenImage.sprite = anyTwoSprite;
+        else if (screenImage.sprite == anyTwoSprite)
+                screenImage.sprite = anyThreeSprite;
+        else if (screenImage.sprite == anyThreeSprite)
+                screenImage.sprite = swipeOneSprite; 
         else if (screenImage.sprite == swipeOneSprite)
             screenImage.sprite = swipeTwoSprite;
         else if (screenImage.sprite == swipeTwoSprite)
-            screenImage.sprite = swipeOneSprite;
+            screenImage.sprite = matchThreeSprite;
+            // auto match to third if second swiped Second profile
     }
     
     // Show match screen corresponding to current swipe screen
     public void ShowMatch()
     {
-        if (screenImage.sprite == swipeOneSprite)
+        if (screenImage.sprite ==anyOneSprite)
+            screenImage.sprite = anyTwoSprite;
+        else if (screenImage.sprite == anyTwoSprite)
+            screenImage.sprite = anyThreeSprite;
+        else if (screenImage.sprite == anyThreeSprite)
+            screenImage.sprite = swipeOneSprite; 
+        else if (screenImage.sprite == swipeOneSprite)
             screenImage.sprite = matchOneSprite;
         else if (screenImage.sprite == swipeTwoSprite)
             screenImage.sprite = matchTwoSprite;
+        // none for three because if two was rejected three was auto matched
     }
 }
 

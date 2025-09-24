e1e2ba437a2d767f3047c349ff89badf8a34b64c
Joshua Li <70135829+tycelm@users.noreply.github.com>
2025-09-24 02:36:46 -0400

    update (#29)
    * Add alarm sfx
    
    loopable, premixed
    
    * Add tutorial music track
    
    * Enable main menu start button
    
    ---------
    
    Co-authored-by: Tristan <64942634+reecpuff@users.noreply.github.com>
    Co-authored-by: Joshua Lenander <52386328+JLenander@users.noreply.github.com>

--- Assets/Scripts/UIScripts/MainMenuHandler.cs
+++ Assets/Scripts/UIScripts/MainMenuHandler.cs
@@ -1,10 +1,12 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UIElements;
 
 namespace UIScripts
 {
 public class MainMenuHandler : MonoBehaviour
 {
+    private Button _startButton;
     private Button _quitButton;
     
     void Start()
@@ -12,13 +14,21 @@ public class MainMenuHandler : MonoBehaviour
         // JQuery like way of retrieving the specific UI elements we care about 
         // https://docs.unity3d.com/6000.2/Documentation/Manual/UIE-UQuery.html
         var root = gameObject.GetComponent<UIDocument>().rootVisualElement;
+        _startButton = root.Query<Button>("StartButton").First();
         _quitButton = root.Query<Button>(name: "QuitButton").First();
         
         // According to https://docs.unity3d.com/Packages/com.unity.inputsystem@1.14/manual/UISupport.html
         // This is how to register the click handler while supporting mouse click and gamepad submit actions
+        _startButton.clicked += StartButtonPressed;
         _quitButton.clicked += QuitButtonPressed;
     }
 
+    private static void StartButtonPressed()
+    {
+        // TODO have a level manager load this so we don't have these strings everywhere
+        SceneManager.LoadScene("CharacterSelect");
+    }
+    
     private static void QuitButtonPressed()
     {
         Debug.Log("QuitButtonPressed");

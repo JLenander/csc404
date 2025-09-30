3985c7f811c3c677c88b9688e168a9f79e6d4716
unknown <j.chen1904@gmail.com>
2025-09-30 18:12:24 -0400

    change back to tutorial

--- Assets/Scripts/GlobalPlayerManager.cs
+++ Assets/Scripts/GlobalPlayerManager.cs
@@ -67,7 +67,7 @@ public class GlobalPlayerManager : MonoBehaviour
                     // pass these players to UI manager
                     GlobalPlayerUIManager.Instance.PassPlayers(_players);
 
-                    SceneManager.LoadScene("Walking");
+                    SceneManager.LoadScene("Tutorial");
                 }
                 else
                 {

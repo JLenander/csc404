1cb484c707b7842023383ab1143a8f66c0400862
Joshua Lenander <42joshl@gmail.com>
2025-10-01 05:33:04 -0400

    Clean up naming

--- Assets/Scripts/GlobalPlayerManager.cs
+++ Assets/Scripts/GlobalPlayerManager.cs
@@ -67,7 +67,7 @@ public class GlobalPlayerManager : MonoBehaviour
                     // pass these players to UI manager
                     GlobalPlayerUIManager.Instance.PassPlayers(_players);
 
-                    SceneManager.LoadScene("TutorialCopy");
+                    SceneManager.LoadScene("TutorialTestGrappleArms");
                 }
                 else
                 {

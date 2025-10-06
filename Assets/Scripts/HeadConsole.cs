41d904735d458887f6ddf9f5d8af54f4f82fbe1b
Joshua Li <70135829+tycelm@users.noreply.github.com>
2025-10-06 00:04:42 -0400

    Dpad to trigger (#44)
    * shoot arm in head
    
    * grapple arm audio code (source tba)
    
    * assigned grapple arm audio
    
    * hand-z dpad to trigger
    
    ---------
    
    Co-authored-by: Viceu <66227626+Viceu@users.noreply.github.com>

--- Assets/Scripts/HeadConsole.cs
+++ Assets/Scripts/HeadConsole.cs
@@ -9,10 +9,72 @@ public class HeadConsole : Interactable
     [SerializeField] private Quaternion camAngle; // current robot head angle
 
     private bool _canInteract = true;
+    
+    [SerializeField] private GameObject leftGrappleArmSpline;
+    [SerializeField] private GameObject rightGrappleArmSpline;
+
+    private bool _leftJammed, _rightJammed;
+    private bool _leftShot, _rightShot;
+    private InputAction _leftTriggerAction, _rightTriggerAction;
+    private GameObject _currPlayer;
+    
+    public AudioSource hookSource;
 
     void Start()
     {
         _splitscreenUIHandler = FindAnyObjectByType<SplitscreenUIHandler>();
+        // for grapple arm
+        _leftJammed = _rightJammed = false;
+        _leftShot = _rightShot = false;
+    }
+    
+    // for grapple arm, check trigger input to shoot or retract
+    void Update()
+    {
+        if (_currPlayer != null)
+        {
+            // Left arm
+            if (_leftTriggerAction != null && _leftTriggerAction.ReadValue<float>() > 0.1f && !_leftJammed)
+            {
+                if (!_leftShot)
+                {
+                    _leftShot = true;
+                    EmergencyEvent.Instance.IncrementCount(true); // or pass correct value
+                    
+                    if (hookSource != null)
+                        hookSource.Play();
+                }
+                leftGrappleArmSpline.GetComponent<SplineController>().SetExtending();
+            }
+            else
+            {
+                if (_leftShot)
+                {
+                    _leftShot = false;
+                }
+                leftGrappleArmSpline.GetComponent<SplineController>().SetRetracting();
+            }
+            
+            // Right arm
+            if (_leftTriggerAction != null && _rightTriggerAction.ReadValue<float>() > 0.1f && !_rightJammed)
+            {
+                if (!_rightShot)
+                {
+                    _rightShot = true;
+                    EmergencyEvent.Instance.IncrementCount(false);
+                    
+                    if (hookSource != null)
+                        hookSource.Play();
+                }
+                rightGrappleArmSpline.GetComponent<SplineController>().SetExtending();
+            }
+            else
+            {
+                if (_rightShot)
+                    _rightShot = false;
+                rightGrappleArmSpline.GetComponent<SplineController>().SetRetracting();
+            }
+        }
     }
     
     public override void Interact(GameObject player)
@@ -23,6 +85,12 @@ public class HeadConsole : Interactable
         player.GetComponent<Player>().TurnOff();
         player.GetComponent<Player>().switchToHead(exteriorCamera);
         _canInteract = false;
+        
+        // for grapple arm
+        _currPlayer = player;
+        var input = _currPlayer.GetComponent<PlayerInput>();
+        _leftTriggerAction = input.actions.FindAction("LeftTrigger");
+        _rightTriggerAction = input.actions.FindAction("RightTrigger");
     }
 
     public override void Return(GameObject player)
@@ -33,13 +101,18 @@ public class HeadConsole : Interactable
         player.GetComponent<Player>().switchOffHead();
 
         _canInteract = true;
+        
+        // for grapple arm
+        _currPlayer = null;
+        _leftTriggerAction = null;
+        _rightTriggerAction = null;
     }
     public override bool CanInteract()
     {
         return _canInteract;
     }
 
-    public void disableInteract()
+    public void DisableInteract()
     {
         _canInteract = false;
         hoverMessage = "[DISABLED] Please Blink";
@@ -47,11 +120,20 @@ public class HeadConsole : Interactable
         outlineColour = new Color(1, 0, 0, 1);
     }
 
-    public void enableInteract()
+    public void EnableInteract()
     {
         _canInteract = true;
         hoverMessage = "Control Head";
         msgColour = new Color(1, 1, 1, 1);
         outlineColour = new Color(1, 1, 1, 1);
+    }
+    
+    // for grapple arm - to be called by HandConsole when arm is broken or fixed
+    public void JamArm(bool left, bool state)
+    {
+        if (left)
+            _leftJammed = state;
+        else
+            _rightJammed = state;
     }
 }

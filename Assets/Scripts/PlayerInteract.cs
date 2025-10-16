cbc57a59b84a769cc28e447fc25e2c9e2c4416e3
unknown <j.chen1904@gmail.com>
2025-10-16 01:09:37 -0400

    resolve conflicts

--- Assets/Scripts/PlayerInteract.cs
+++ Assets/Scripts/PlayerInteract.cs
@@ -43,15 +43,13 @@ public class PlayerInteract : MonoBehaviour
                 Debug.Log("Interacting with " + currentItem);
                 interacting = currentItem;
                 currentItem.Interact(gameObject);
-<<<<<<< HEAD
-=======
+
                 DisableCurrInteractable();
 
                 if (GlobalPlayerUIManager.Instance != null)
                 {
                     GlobalPlayerUIManager.Instance.EnableScreenGreyscale(playerId);
                 }
->>>>>>> 8608942 (transparent black screen overlay)
             }
         }
 
@@ -67,21 +65,14 @@ public class PlayerInteract : MonoBehaviour
 
     void CheckInteraction()
     {
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> d40fb4e (interaction group)
+
         if (interacting != null)
         {
             SetReturnText(interacting);
             return;
         }
-<<<<<<< HEAD
-=======
         if (interacting != null) return;
->>>>>>> 8608942 (transparent black screen overlay)
-=======
->>>>>>> d40fb4e (interaction group)
+
 
         RaycastHit hit;
 
@@ -129,13 +120,10 @@ public class PlayerInteract : MonoBehaviour
             GlobalPlayerUIManager.Instance.EnableInteractionText(playerId, "To Return", currItem.msgColour);
     }
 
-<<<<<<< HEAD
     /// <summary>
     /// Visually enable the interaction prompt for the Interactable object newInteractable that this player is looking at
     /// </summary>
     /// <param name="newInteractable"></param>
-=======
->>>>>>> d40fb4e (interaction group)
     void SetNewCurrInteractable(Interactable newInteractable)
     {
         currentItem = newInteractable;
